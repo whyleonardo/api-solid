9ef3a6fec062fddec44d90f75a0e0e82202365a8
Christian Leonardo <pleasefloyd73@gmail.com>
2024-07-18 17:29:13 -0300

    chore: update prisma scripts

---
+++ prisma/migrations/20240718202709_create_users/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_id_key" ON "users"("id");
