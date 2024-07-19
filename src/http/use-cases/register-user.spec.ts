a3be02f57d276785273120ceb908b6b34cca6972
Christian Leonardo <pleasefloyd73@gmail.com>
2024-07-19 17:33:39 -0300

    refactor: update imports with alias

--- src/http/use-cases/register-user.spec.ts
+++ src/http/use-cases/register-user.spec.ts
@@ -1,7 +1,7 @@
+import { UserAlreadyExistsError } from "@/http/errors/user-already-exists-error";
 import { InMemoryUsersRepository } from "@/repositories/in-memory/in-memory-users-repository";
 import { compare } from "bcryptjs";
 import { describe, expect, it } from "vitest";
-import { UserAlreadyExistsError } from "../errors/user-already-exists-error";
 import { RegisterUserUseCase } from "./register-user";
 
 describe("Register User Use Case", () => {
