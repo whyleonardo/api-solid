06167f40150fc4d99bd335fe4e9b13f741e81554
Christian Leonardo <pleasefloyd73@gmail.com>
2024-07-23 14:40:32 -0300

    refactor: update interface name

--- src/http/use-cases/register-user.ts
+++ src/http/use-cases/register-user.ts
@@ -1,40 +1,40 @@
-import { UserAlreadyExistsError } from "@/http/errors/user-already-exists-error";
-import type { UsersRepository } from "@/repositories/users-repository";
-import type { User } from "@prisma/client";
-import bcrypt from "bcryptjs";
+import { UserAlreadyExistsError } from "@/http/errors/user-already-exists-error"
+import type { UsersRepository } from "@/repositories/users-repository"
+import type { User } from "@prisma/client"
+import bcrypt from "bcryptjs"
 
 interface RegisterUserUseCaseProps {
-  name: string;
-  email: string;
-  password: string;
+	name: string
+	email: string
+	password: string
 }
 
-interface RegisterUseCaseResponse {
-  user: User;
+interface RegisterUserUseCaseResponse {
+	user: User
 }
 
 export class RegisterUserUseCase {
-  constructor(private usersRepository: UsersRepository) {}
+	constructor(private usersRepository: UsersRepository) {}
 
-  async execute({
-    email,
-    name,
-    password,
-  }: RegisterUserUseCaseProps): Promise<RegisterUseCaseResponse> {
-    const password_hash = await bcrypt.hash(password, 6);
+	async execute({
+		email,
+		name,
+		password
+	}: RegisterUserUseCaseProps): Promise<RegisterUserUseCaseResponse> {
+		const password_hash = await bcrypt.hash(password, 6)
 
-    const userWithSameEmail = await this.usersRepository.findByEmail(email);
+		const userWithSameEmail = await this.usersRepository.findByEmail(email)
 
-    if (userWithSameEmail) {
-      throw new UserAlreadyExistsError();
-    }
+		if (userWithSameEmail) {
+			throw new UserAlreadyExistsError()
+		}
 
-    const user = await this.usersRepository.create({
-      email,
-      password_hash,
-      name,
-    });
+		const user = await this.usersRepository.create({
+			email,
+			password_hash,
+			name
+		})
 
-    return { user };
-  }
+		return { user }
+	}
 }
