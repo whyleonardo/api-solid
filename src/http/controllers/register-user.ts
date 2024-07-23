0a1ac58ab6e1832a4d44827d2e9b6e0947d0bb41
Christian Leonardo <pleasefloyd73@gmail.com>
2024-07-23 10:03:40 -0300

    refactor: apply factory pattern

--- src/http/controllers/register-user.ts
+++ src/http/controllers/register-user.ts
@@ -1,39 +1,37 @@
-import { RegisterUserUseCase } from "@/http/use-cases/register-user";
-import { PrismaUsersRepository } from "@/repositories/prisma/prisma-users-repository";
-import type { FastifyReply, FastifyRequest } from "fastify";
-import { z } from "zod";
-import { UserAlreadyExistsError } from "../errors/user-already-exists-error";
+import { makeRegisterUserUseCase } from "@/http/use-cases/factories/make-register-user-use-case"
+import type { FastifyReply, FastifyRequest } from "fastify"
+import { z } from "zod"
+import { UserAlreadyExistsError } from "../errors/user-already-exists-error"
 
 export const registerUser = async (
-  request: FastifyRequest,
-  reply: FastifyReply
+	request: FastifyRequest,
+	reply: FastifyReply
 ) => {
-  const registerUserBodySchema = z.object({
-    name: z.string(),
-    email: z.string().email(),
-    password: z
-      .string()
-      .min(6, { message: "Password must contain at least 6 character(s)" }),
-  });
+	const registerUserBodySchema = z.object({
+		name: z.string(),
+		email: z.string().email(),
+		password: z
+			.string()
+			.min(6, { message: "Password must contain at least 6 character(s)" })
+	})
 
-  const { email, name, password } = registerUserBodySchema.parse(request.body);
+	const { email, name, password } = registerUserBodySchema.parse(request.body)
 
-  try {
-    const prismaUsersRepository = new PrismaUsersRepository();
-    const registerUserUseCase = new RegisterUserUseCase(prismaUsersRepository);
+	try {
+		const registerUserUseCase = makeRegisterUserUseCase()
 
-    await registerUserUseCase.execute({
-      email,
-      name,
-      password,
-    });
-  } catch (err) {
-    if (err instanceof UserAlreadyExistsError) {
-      return reply.status(409).send({ message: err.message });
-    }
+		await registerUserUseCase.execute({
+			email,
+			name,
+			password
+		})
+	} catch (err) {
+		if (err instanceof UserAlreadyExistsError) {
+			return reply.status(409).send({ message: err.message })
+		}
 
-    throw err;
-  }
+		throw err
+	}
 
-  return reply.status(201).send();
-};
+	return reply.status(201).send()
+}
