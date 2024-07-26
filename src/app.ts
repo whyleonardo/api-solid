674854d3ca62f3b4e7077f9fb5fefe111084b4c5
Christian Leonardo <pleasefloyd73@gmail.com>
2024-07-26 14:39:51 -0300

    feat: implement refresh token

--- src/app.ts
+++ src/app.ts
@@ -2,19 +2,28 @@ import { env } from "@/env"
 import { checkInsRoutes } from "@/http/controllers/check-ins/routes"
 import { gymsRoutes } from "@/http/controllers/gyms/routes"
 import { usersRoutes } from "@/http/controllers/users/routes"
+import fastifyCookie from "@fastify/cookie"
 import fastifyJWT from "@fastify/jwt"
 import fastify from "fastify"
 import { ZodError } from "zod"
 
 export const app = fastify()
 
+app.register(fastifyJWT, {
+	secret: env.JWT_SECRET,
+	cookie: {
+		cookieName: "refreshToken",
+		signed: false
+	},
+	sign: {
+		expiresIn: "10m"
+	}
+})
+
+app.register(fastifyCookie)
 app.register(gymsRoutes)
 app.register(usersRoutes)
 app.register(checkInsRoutes)
-
-app.register(fastifyJWT, {
-	secret: env.JWT_SECRET
-})
 
 app.setErrorHandler((error, _request, reply) => {
 	if (error instanceof ZodError) {
--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -2,15 +2,16 @@ import type { FastifyInstance } from "fastify"
 
 import { verifyJWT } from "@/http/middlewares/verify-jwt"
 
-import { profile } from "@/http/controllers/users/profile"
-import { registerUser } from "@/http/controllers/users/register-user"
 import { authenticate } from "./authenticate"
+import { profile } from "./profile"
+import { refresh } from "./refresh"
+import { registerUser } from "./register-user"
 
 export async function usersRoutes(app: FastifyInstance) {
 	app.post("/users", registerUser)
 	app.post("/sessions", authenticate)
 
-	// app.patch("/token/refresh", refresh)
+	app.patch("/token/refresh", refresh)
 
 	/** Authenticated */
 	app.get("/me", { onRequest: [verifyJWT] }, profile)
